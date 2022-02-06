474ebf40699f761516a6a04eb067d320cff5ae92
geekbrains <phantom335@mail.ru>
2022-02-06 23:24:28 +0300

    dz7

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -34,6 +34,22 @@ class User extends Authenticatable
             <input type="submit">
         </div>
     </form>
+    namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+
+class Category extends Model
+{
+    public function getList ()
+    {
+        $categories_table = DB::table('category_news');
+        return $categories_table->select(['id', 'title'])
+                        ->get()
+                        ->pluck('title', 'id');
+    }
+}
     
     @endsection 
 }
